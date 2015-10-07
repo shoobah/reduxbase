110ce59a199774b2c1ef86a989152c414e86b3b1
Anders Sjöberg <shoobah@gmail.com>
2015-10-07 21:20:43 +0200

    Lite modifiering

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, ja jävlar!.</h1>
+      <h1>Superlive!!</h1>
     );
   }
 }
